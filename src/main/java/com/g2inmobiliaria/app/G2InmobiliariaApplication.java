effff5c07ee5c179e687b9ed7ded6a76ad13e97e
DylanArias0709 <dylan.arias.duran@est.una.ac.cr>
2024-05-08 08:48:28 -0600

    Se agrego el proyecto

---
+++ src/main/java/com/g2inmobiliaria/app/G2InmobiliariaApplication.java
@@ -0,0 +1,13 @@
+package com.g2inmobiliaria.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G2InmobiliariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G2InmobiliariaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/g2inmobiliaria/app/G2InmobiliariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g2inmobiliaria.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G2InmobiliariaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
